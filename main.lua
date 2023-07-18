2bc091dd0d5213b6816c7f10b50a6bdb2726605e
John Hackett <john@mutual.email>
2023-07-18 18:16:49 +0100

    url bar works! huzzah.

--- main.lua
+++ main.lua
@@ -5,6 +5,7 @@ local https = require("ssl.https")
 local lpeg = require("lpeg")
 local re = require("re")
 local InputField = require("InputField")
+local inspect = require("inspect")
 
 local url = "https://www.theguardian.com/education/2023/jul/14/rishi-sunak-force-english-universities-cap-low-value-degrees"
 local field      = InputField(url)
@@ -12,26 +13,26 @@ local field      = InputField(url)
 function fetch_and_build(url) 
   local page, code, headers, status = https.request(url)
 
-local doc = xmlua.HTML.parse(page)
+  local doc = xmlua.HTML.parse(page)
 
--- grab the headline
-local headline = doc:css_select('div [data-gu-name="headline"]')
-local headline = headline:css_select('h1')
+  -- grab the headline
+  local headline = doc:css_select('div [data-gu-name="headline"]')
+  local headline = headline:css_select('h1')
 
--- grab the standfirst/subheading
-local standfirst = doc:css_select('div [data-gu-name="standfirst"]')
-local standfirst = standfirst:css_select('p')
+  -- grab the standfirst/subheading
+  local standfirst = doc:css_select('div [data-gu-name="standfirst"]')
+  local standfirst = standfirst:css_select('p')
 
--- grab the byline (as in, line denoting who the article is by!)
-local byline = doc:css_select('[data-link-name="byline"]')
-local byline = byline:css_select('div')
+  -- grab the byline (as in, line denoting who the article is by!)
+  local byline = doc:css_select('[data-link-name="byline"]')
+  local byline = byline:css_select('div')
 
--- grab the content
-local content = doc:css_select('div[id~="maincontent"]')
-local content = content:to_xml():gsub("<figure(.-)</figure>","") -- nuke everything inside the figure tag as it is newsletter begging
-local content = xmlua.XML.parse(content)
-local content = content:css_select('p') -- select only p tags
-local content = content:to_xml():gsub("<p(.-)>","\n"):gsub("</p>","\n") -- we're "rendering" with printf, so just replace paragraph tags with newlines
+  -- grab the content
+  local content = doc:css_select('div[id~="maincontent"]')
+  local content = content:to_xml():gsub("<figure(.-)</figure>","") -- nuke everything inside the figure tag as it is newsletter begging
+  local content = xmlua.XML.parse(content)
+  local content = content:css_select('p') -- select only p tags
+  local content = content:to_xml():gsub("<p(.-)>","\n"):gsub("</p>","\n") -- we're "rendering" with printf, so just replace paragraph tags with newlines
 
   return headline, standfirst, byline, content
 end
@@ -51,13 +52,17 @@ end
 local fieldX = 1
 local fieldY = 1
 
-headline, standfirst, byline, content = fetch_and_build(url)
+headline, standfirst, byline, content = fetch_and_build(field.text)
 
 love.keyboard.setKeyRepeat(true)
 
+function enterkey()
+  
+end
+
 function love.keypressed(key, scancode, isRepeat)
-    if key == "enter" then
-      field:enterkey()
+    if key == "return" then
+      headline, standfirst, byline, content = fetch_and_build(field.text)
     else
 	  field:keypressed(key, isRepeat)
 	end
@@ -97,5 +102,5 @@ function love.draw()
 
   love.graphics.setBackgroundColor( 0,0,0, 0 )
   love.graphics.setColor(1, 1, 1, 1)
-    love.graphics.printf(headline:content().."\n\n"..standfirst:content().."\n\n"..byline:content().."\n\n"..content, padding_left, 30, padding_right, "left")
+  love.graphics.printf(headline:content().."\n\n"..standfirst:content().."\n\n"..byline:content().."\n\n"..content, padding_left, 30, padding_right, "left")
 end
