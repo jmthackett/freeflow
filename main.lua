418241a7aedf10a4fd7f75e8d9644ef1c8c5fe7d
John Hackett <john@mutual.email>
2023-07-24 08:59:22 +0100

    lots of things: mostly query editor and refinements to the page parsing function

--- main.lua
+++ main.lua
@@ -1,5 +1,5 @@
-
-
+local stringx = require("pl.stringx")
+local tablex = require("pl.tablex")
 local luacs = require("luacs")
 local xmlua = require("xmlua")
 local https = require("ssl.https")
@@ -7,16 +7,19 @@ local lpeg = require("lpeg")
 local re = require("re")
 local InputField = require("InputField")
 local inspect = require("inspect")
+local math = require("math")
+
+stringx.import()
 
 local LG = love.graphics
 local LK = love.keyboard
 
+local page_xml = "<example>This is the XML pane</example>"
+local query = "//div[@role='main']//h2/a[@href]/@href | //div[@role='main']//h2/a[@href]/text() | //div[@role='main']//div[@aria-hidden='true']/p | //div[@role='main']//h3/a[@href]/text()"
 local url = "https://www.theguardian.com/education/2023/jul/14/rishi-sunak-force-english-universities-cap-low-value-degrees"
-
-headline = ""
-byline = ""
-standfirst = ""
-content = "This is the Freeflow home page"
+local url = "https://www.theguardian.com/politics/2023/jul/19/sunak-braverman-and-city-regulator-wade-into-farage-banking-row"
+local url = "https://www.lrb.co.uk/"
+local content = "This is the Freeflow home page"
 
 font_size=12
 
@@ -51,10 +54,31 @@ local textInputs = {
 		),
 		x         = 31,
 		y         = 31,
-		width     = w - 60,
-		height    = h - 30,
+		width     = (w - 60),
+		height    = h - 60,
 		alignment = "left",
 		setEditable = false,
+		font_size = 12,
+	},
+	{
+		field = InputField(page_xml,
+			"multiwrap"
+		),
+		x         = w / 2,
+		y         = 31,
+		width     = (w - 60) / 2,
+		height    = h - 60,
+		alignment = "left",
+		setEditable = false,
+		font_size = 12,
+	},
+	{
+		field     = InputField(query),
+		x         = 1,
+		y         = h - 30,
+		width     = w,
+		height    = 30,
+		alignment = "left",
 		font_size = 12,
 	},
 }
@@ -81,31 +105,42 @@ local function getTextInputAtCoords(x, y)
 	return nil -- No text input at coords.
 end
 
-function fetch_and_build(url) 
+function fetch_and_build(url, query) 
+  print("Fetching url:"..url)
   local page, code, headers, status = https.request(url)
-
   local doc = xmlua.HTML.parse(page)
-
-  -- grab the headline
-  local headline = doc:css_select('div [data-gu-name="headline"]')
-  local headline = headline:css_select('h1')
-
-  -- grab the standfirst/subheading
-  local standfirst = doc:css_select('div [data-gu-name="standfirst"]')
-  local standfirst = standfirst:css_select('p')
-
-  -- grab the byline (as in, line denoting who the article is by!)
-  local byline = doc:css_select('[rel="author"]')
-  local byline = byline:css_select('div')
-
-  -- grab the content
-  local content = doc:css_select('div[id~="maincontent"]')
-  local content = content:to_xml():gsub("<figure(.-)</figure>","") -- nuke everything inside the figure tag as it is newsletter begging
-  local content = xmlua.XML.parse(content)
-  local content = content:css_select('p') -- select only p tags
-  local content = content:to_xml():gsub("<p(.-)>","\n"):gsub("</p>","\n") -- we're "rendering" with printf, so just replace paragraph tags with newlines
-
-  return headline:content(), standfirst:content(), byline:content(), content
+  local content = doc:root():search(query)
+  print(content:paths())
+  print(content:content())
+--  print(type(content))
+--  print(inspect(content))
+--  print(inspect(content:paths()))
+  local result_content = ""
+  local result_xml = ""
+  
+  local length = 0
+  local qlength = stringx.split(query,'|')
+  for i, t in ipairs(qlength) do -- there must be a better way to get the number of elements in a table, surely?
+    length = i
+  end
+  
+  for i, t in ipairs(content) do
+    if math.fmod(i,length) == 0 then
+      result_content = result_content .. content[i]:path() .. "\n\n" .. content[i]:content() .. "\n\n-----------\n\n"
+    else
+      result_content = result_content .. content[i]:path() .. "\n\n" .. content[i]:content() .. "\n\n"
+    end
+  end
+  
+  for i, t in ipairs(content) do
+    if content[i].to_html == nil then
+      print("Skipped converting to html due to error")
+    else
+      result_xml = result_xml .. content[i]:path() .. "\n\n" .. content[i]:to_html() .. "\n\n"
+    end
+  end
+  
+  return result_content, result_xml
 end
 
 local x, y, w, h = love.window.getSafeArea( )
@@ -153,12 +188,13 @@ function love.keypressed(key, scancode, isRepeat)
 		focusedTextInput = textInputs[i]
 		focusedTextInput.field:resetBlinking()
 	elseif key == "return" and textInputs[1] then 
-	    headline, standfirst, byline, content = fetch_and_build(url)
-	    textInputs[2].field.text = ""..headline.."\n\n"..standfirst.."\n\n"..byline.."\n\n"..content..""
-
-    elseif key == "lctrl" and textInputs[2] then
-         font_size = font_size+1
-
+	    if textInputs[4].field.text == "" then
+	      page_content, page_xml = fetch_and_build(textInputs[1].field.text,"//body")
+	    else
+	      page_content, page_xml = fetch_and_build(textInputs[1].field.text,textInputs[4].field.text)
+	    end
+	    textInputs[2].field.text = page_content
+	    textInputs[3].field.text = page_xml
 	-- Then handle focused InputField (if there is one).
 	elseif focusedTextInput and focusedTextInput.field:keypressed(key, isRepeat) then
 		-- Event was handled.
@@ -223,24 +259,48 @@ function love.update(dt)
 	if focusedTextInput then
 		focusedTextInput.field:update(dt)
 	end
+
+-- try to cut down on drawing    
+    x, y, w, h = love.window.getSafeArea( )
+    if dt < 1/4 then
+        love.timer.sleep(1/4 - dt)
+    end
+    print("Update running - dt: " .. dt)
+    print("Dumping x: "..x..", y:"..y..", w:"..w..", h:"..h.."")
 end
 
+
+
 function love.draw()
-  x, y, w, h = love.window.getSafeArea( )
 
   local extraFont = LG.newFont(18)
+  local newx, newy, neww, newh = love.window.getSafeArea( )
+  if x ~= newx then
+    print("x hasn't changed, not redrawing")
+  else
+    textInputs[1].x = 1
+    textInputs[1].y = 1
+    textInputs[1].width = w
+    textInputs[1].height = 30
 
-  textInputs[1].x = 1
-  textInputs[1].y = 1
-  textInputs[1].width = w
-  textInputs[1].height = 30
-
-  textInputs[2].x = (w / 100) * 15
-  textInputs[2].y = 31
-  textInputs[2].width = (w / 100) * 70
-  textInputs[2].height = h - 30
-  textInputs[2].field:setEditable(false)
-  love.graphics.setBackgroundColor( 0,0,0, 1 )
+    textInputs[2].x = (w / 100) * 15 
+    textInputs[2].y = 31
+    textInputs[2].width = (w / 100) * 35
+    textInputs[2].height = h - 60
+    textInputs[2].field:setEditable(false)
+  
+    textInputs[3].x = (w / 100) * 60
+    textInputs[3].y = 31
+    textInputs[3].width = (w / 100) * 35
+    textInputs[3].height = h - 60
+    textInputs[3].field:setEditable(false)
+  
+    textInputs[4].x = 1
+    textInputs[4].y = h - 30
+    textInputs[4].width = w
+    textInputs[4].height = 30
+  end
+  love.graphics.setBackgroundColor( 0,0,0, 0.8 )
   love.graphics.setColor(1, 1, 1, 0.5)
   LG = love.graphics
      for i, textInput in ipairs(textInputs) do
@@ -341,7 +401,7 @@ function love.draw()
 		end
 	end
 
-	--love.timer.sleep( 0.5)
+--	love.timer.sleep( 0.1 )
 	--LG.print(content, 3, LG.getHeight()-3-2*extraFont:getHeight())
 --  love.graphics.printf(headline.."\n\n"..standfirst.."\n\n"..byline.."\n\n"..content, padding_left, math.min(vely,30), padding_right, "left")
 end
