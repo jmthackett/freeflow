628b05e989f10e33e73df727de2a58151eb40771
John Hackett <john@mutual.email>
2023-07-27 22:53:19 +0100

    working sketch of opengraph parsing

--- opengraph.lua
+++ opengraph.lua
@@ -3,19 +3,16 @@ local xmlua = require("xmlua")
 local https = require("ssl.https")
 local inspect = require("inspect")
 
-local page, code, headers, status = https.request("https://www.lrb.co.uk/feeds/rss")
-local doc = xmlua.HTML.parse(page)
-local docsearch = doc:search("//item")
-print(type(docsearch))
-
---[[
-//meta[@property='og:title']/@content
-//meta[@property='og:type']/@content
-//meta[@property='og:image']/@content
-//meta[@property='og:url']/@content
-//meta[@property='og:description']/@content
-//meta[@property='og:site_name']/@content
-//meta[@property='og:locale']/@content
-//meta[@property='og:article:published_time']/@content
-//meta[@property='og:article:modified_time']/@content
+opengraph = [[
+//meta[contains(@property,"og:")]
 ]]
+
+local page, code, headers, status = https.request("https://www.theguardian.com/books/2023/jul/27/big-birmingham-bookshop-crawl-booksellers-thriving")
+local doc = xmlua.HTML.parse(page)
+local docsearch = doc:search(opengraph)
+print(type(docsearch))
+print(inspect(docsearch))
+
+for i,t in ipairs(docsearch) do
+  print(docsearch[i]:get_attribute("property"):gsub("og:","") .. ": " .. docsearch[i]:get_attribute("content"))
+end
