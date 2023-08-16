11c7c24c36345519a867ede33ddff67545300ac0
John Hackett <john@mutual.email>
2023-08-16 23:01:22 +0100

    using ordered map in check.lua

--- ci/check.lua
+++ ci/check.lua
@@ -1,15 +1,17 @@
 #!/usr/bin/env luajit
 
+local json = require("rapidjson")
 local luacs = require("luacs")
 local xmlua = require("xmlua")
 local https = require("ssl.https")
 local inspect = require("inspect")
+local OrderedMap = require('pl.OrderedMap')
 
 
+site = OrderedMap()
 local protocol = "https://"
-local site = {}
 local verbose = false
-site['name'] = arg[1]
+site:set('name', arg[1])
 
 -- fetch specified page
 local page, code, headers, status = https.request(protocol .. arg[1])
@@ -20,12 +22,13 @@ local err, doc = pcall(function() return xmlua.HTML.parse(page) end)
 --end
 
 -- test for RSS
+site:set('rss', false)
 -- TODO: hbrchina.org does this <a href="http://app.hbrchina.org/rss.php" target"_blank">RSS</a> which needs catching if possible
 -- TODO: technologyreview.com has a massive horrendous json blob containing this: {"displayIcon":true,"type":"rss","url":"https://www.technologyreview.com/feed/","themeName":"footer","context":"footer"}
 local docsearch = doc:search("//link[contains(@type,'application/rss+xml')]")
 local err, val = pcall(function() return docsearch[1]:get_attribute("href") end)
 if (err ~= false) then
-  site['rss'] = val
+  site:set('rss', val)
   if (verbose) then
     print(val)
   end
@@ -36,10 +39,10 @@ if (code == 200) then
   if (verbose) then
     print("RSS found at " .. arg[1] .. "/rss")
   end
-  site['rss'] = "" .. arg[1] .. "/rss"
+  site:set('rss', "" .. arg[1] .. "/rss")
 end
-
 -- test for opengraph
+site:set('opengraph', false)
 local docsearch = doc:search("//meta[contains(@property,'og:')]")
 local err, val = pcall(function() return docsearch[1]:get_attribute("property") end)
 if (err ~= false) then
@@ -48,36 +51,40 @@ if (err ~= false) then
       print("OpenGraph property found - " .. docsearch[i]:get_attribute("property"):gsub("og:","") .. ": " .. docsearch[i]:get_attribute("content"))
     end
   end
-  site['opengraph'] = true
+  site:set('opengraph', true)
 end
-
 -- test for html5 article tags
+site:set('html5_article', false)
 local docsearch = doc:search("//article")
 local err, val = pcall(function() return docsearch[1]:name() end)
 if (err ~= false) then
-  site['html5_article'] = true
+  site:set('html5_article', true)
   if (verbose) then
     print(val)
   end
 end
 
 -- test for html5 subsection
+site:set('html_subsection', false)
 local docsearch = doc:search("//subsection")
 local err, val = pcall(function() return docsearch[1]:name() end)
 if (err ~= false) then
-  site['html5_subsection'] = true
+  site:set('html5_subsection', true)
   if (verbose) then
      print(val)
   end
 end
 
 -- test for aria tags
+site:set('aria', false)
+site:set('aria_label', false)
+site:set('aria_description', false)
 -- TODO: map aria annotations to relevant roles
 local docsearch = doc:search("//*[@aria-description]")
 local err, val = pcall(function() return docsearch[1]:name() end)
 if (err ~= false) then
-  site['aria_description'] = true
-  site['aria'] = true
+  site:set('aria_description', true)
+  site:set('aria', true)
   if (verbose) then
     print(val)
   end
@@ -86,14 +93,15 @@ end
 local docsearch = doc:search("//*[@aria-label]")
 local err, val = pcall(function() return docsearch[1]:name() end)
 if (err ~= false) then
-  site['aria_label'] = true
-  site['aria'] = true
+  site:set('aria_label', true)
+  site:set('aria', true)
   if (verbose) then
     print(val)
   end
 end
 
 -- test for sitemap.xml
+site:set('sitemap', false)
 -- TODO: check the url ends in a '/' and add one otherwise
 -- TODO: also check the fqdn as a base path in cases where we have a path or a redirect to a path that isn't '/'
 local page, code, headers, status = https.request(protocol .. arg[1] .. "/sitemap")
@@ -101,7 +109,7 @@ if (code == 200) then
   if (verbose) then
     print("Sitemap found at /sitemap")
   end
-  site['sitemap'] = 'sitemap'
+  site:set('sitemap', 'sitemap')
 end
 
 local page, code, headers, status = https.request(protocol .. arg[1] .. "/sitemap.xml")
@@ -109,7 +117,7 @@ if (code == 200) then
   if (verbose) then
     print("Sitemap found at /sitemap.xml")
   end
-  site['sitemap'] = 'sitemap.xml'
+  site:set('sitemap', 'sitemap.xml')
 end
 
 local page, code, headers, status = https.request(protocol .. arg[1] .. "/sitemap_index.xml")
@@ -117,9 +125,25 @@ if (code == 200) then
   if (verbose) then
     print("Sitemap found at /sitemap_index.xml")
   end
-  site['sitemap'] = 'sitemap_index.xml'
+  site:set('sitemap', 'sitemap_index.xml')
 end
 
-print(inspect(site))
+--print(inspect(site))
+--result = json.encode(site)
+--print(result)
+mdoutput = ""
+for k, v in site:iter() do
+  if k == 'name' then
+    mdoutput = "| " .. v .. " | "
+  else
+    if v ~= false then
+      mdoutput = mdoutput .. k ..  " ✅ | "
+    else
+      mdoutput = mdoutput .. k .. " ❌ | "
+    end
+  end
+end
+
+print(mdoutput)
 
 return 0
