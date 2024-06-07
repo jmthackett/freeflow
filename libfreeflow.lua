eeb3b142ca9f431f967ce0e30a0c72a92d868d72
John Hackett <john@mutual.email>
2024-06-07 16:59:45 +0100

    Added sketch uri functions; re-ordered evaluation of page elements; temporarily disabled opengraph

--- libfreeflow.lua
+++ libfreeflow.lua
@@ -12,17 +12,27 @@ local inspect = require("inspect")
 local math = require("math")
 local url_parser = require("net.url")
 local sqlite = require("lsqlite3complete")
---
+local Logging = require("logging.console")
 
 local db = "sites.db" 
 
+function check_uri(uri)
+  return uri
+end
+
+function absolute_uri(fqdn, path)
+  return fqdn.."/"..path
+end
+
 function fetch_and_build(url) 
+  local logger = Logging.new()
+  logger:setLevel(logger.OFF)
   local result_content = ""
   local result_xml = ""
   local buttons = {}
 
   -- fetch the page and parse it
---  print("Fetching url:"..url)
+  logger:info("Fetching url: "..url)
   local db = "sites.db" 
   local page, code, headers, status = https.request(url)
   local doc = xmlua.HTML.parse(page)
@@ -30,14 +40,14 @@ function fetch_and_build(url)
   -- try common approaches
   
   -- rss: if we find rss, let's assume we're on an index page and skip everything else
+  logger:info("Searching for RSS")
   local docsearch = doc:search("//link[contains(@type, 'application/rss+xml')]")
-  print(#docsearch)
   local err, val = pcall(function() return docsearch[1]:get_attribute("href") end)
   
   if (#docsearch ~= 0) then
-    print("Found RSS!")
+    logger:info("Found RSS: "..val)
+    -- TODO: use check_uri to see if this is an absolute path and absolute_uri to fix it if not
     local rss = https.request(val)
-    print("Fetched RSS!")
     local rss_doc = xmlua.XML.parse(rss)
     local rss_search = rss_doc:search("//item")
     local rss_content = ""
@@ -56,32 +66,107 @@ function fetch_and_build(url)
         rss_content = rss_content .. val:content() .. "\n"
 --      end
     end
-
+    logger:info("Returning parsed RSS")
     return rss_content, rss
   end
 
-  -- no joy? let's see if we have a query in the database that tells us how to handle this page
-  print("Pre-query (sqlite)")
-  local query, _, _ = find_query(url,db)
-  if (query == nil and result_content == "") then
-    content = "Sorry, we don't know how to display this page yet! Here's what we've got:"
-    return result_content, ""
-  end
---  print("QUERY:" .. query)
-  print("Post-query (sqlite)")
---  print("Pre-query (xmlua)")
-  local content = doc:root():search(query)
---  print("Post-query (xmlua)")
+  logger:info("Checking for a site specific query")
+  local status, query = pcall(function()
+      return find_query(url, db)
+  end)
+
+  if (query == nil) then
+    logger:info("No query found, attempting guesswork")
+
+  -- test for html5 article tags
+    logger:info("Testing for HTML5 article tags")
+    local docsearch = doc:search("//article//p")
+    local err, val = pcall(function() return docsearch[1]:name() end)
+    if (#docsearch ~= 0) then
+      logger:info("Found HTML5 article tags")
+      for i,t in ipairs(docsearch) do
+        result_content = result_content .. docsearch[i]:content() .. "\n\n"
+      end
+      return result_content, ""
+    end
   
+  -- test for html5 subsection
+    local docsearch = doc:search("//subsection")
+    local err, val = pcall(function() return docsearch[1]:name() end)
+    if (#docsearch ~= 0) then
+      logger:info("Found HTML5 article tags")
+      for i,t in ipairs(docsearch) do
+        result_content = result_content .. docsearch[i]:content() .. "\n\n"
+      end
+      return result_content, ""
+    end
+  
+  -- test for aria tags
+  -- TODO: map aria annotations to relevant roles
+    local docsearch = doc:search("//*[@aria-description]")
+    local err, val = pcall(function() return docsearch[1]:name() end)
+    if (#docsearch ~= 0) then
+      logger:info("Found HTML5 article tags")
+      for i,t in ipairs(docsearch) do
+        result_content = result_content .. docsearch[i]:content() .. "\n\n"
+      end
+      return result_content, ""
+    end
+  
+    local docsearch = doc:search("//*[@aria-label]")
+    local err, val = pcall(function() return docsearch[1]:name() end)
+    if (#docsearch ~= 0) then
+      logger:info("Found HTML5 article tags")
+      for i,t in ipairs(docsearch) do
+        result_content = result_content .. docsearch[i]:content() .. "\n\n"
+      end
+      return result_content, ""
+    end
+
+    -- opengraph
+    logger:info("Checking for OpenGraph properties")
+    local docsearch = doc:search("//meta[contains(@property,'og:')]")
+    local err, val = pcall(function() return docsearch[1]:get_attribute("property") end)
+    if (#docsearch ~= 0) then
+    -- we need to parse this
+      for i,t in ipairs(docsearch) do
+
+        if (docsearch[i]:get_attribute("property"):gsub("og:","") == "title") then
+          logger:info("OpenGraph results[title]: "..result_content)
+          result_content = result_content .. docsearch[i]:get_attribute("content") .. "\n\n"
+        end
+      
+        if (docsearch[i]:get_attribute("property"):gsub("og:","") == "description") then
+          logger:info("OpenGraph results[description]: "..result_content)
+          result_content = result_content .. docsearch[i]:get_attribute("content") .. "\n\n"
+        end
+      
+        if (docsearch[i]:get_attribute("property"):gsub("og:","") == "image") then
+          logger:info("OpenGraph results[image]: "..result_content)
+          result_content = result_content .. docsearch[i]:get_attribute("content") .. "\n\n"
+        end
+      
+        if (docsearch[i]:get_attribute("property"):gsub("og:","") == "url") then
+          logger:info("OpenGraph results[url]: "..result_content)
+          result_content = result_content .. docsearch[i]:get_attribute("content") .. "\n\n"
+        end
+      end
+      return result_content, ""
+    end
+
+    return result_content, result_xml
+  end
+
+  logger:info("Attempting query")
+
+  local content = doc:root():search(query)
   local length = 0
   local query_length = 0
-  print("pre-split")
   if string.match(query, '|') then
-      query_length = stringx.split(query,'|')
-      print("post-split")
-      for i, t in ipairs(query_length) do -- there must be a better way to get the number of elements in a table, surely?
-          length = i
-      end
+    query_length = stringx.split(query,'|')
+    for i, t in ipairs(query_length) do -- there must be a better way to get the number of elements in a table, surely?
+      length = i
+    end
   end
   
   for i, t in ipairs(content) do
@@ -103,86 +188,19 @@ function fetch_and_build(url)
   if (result_content) then  
     return result_content, result_xml
   end
-  -- opengraph
-  local docsearch = doc:search("//meta[contains(@property,'og:')]")
-  local err, val = pcall(function() return docsearch[1]:get_attribute("property") end)
-  if (#docsearch ~= 0) then
-  -- we need to parse this
-    for i,t in ipairs(docsearch) do 
 
-      print("OG results:")
-      print(result_content)
-
-      if (docsearch[i]:get_attribute("property"):gsub("og:","") == "title") then
-        result_content = result_content .. docsearch[i]:get_attribute("content") .. "\n\n"
-      end
-      
-      if (docsearch[i]:get_attribute("property"):gsub("og:","") == "description") then
-        result_content = result_content .. docsearch[i]:get_attribute("content") .. "\n\n"
-      end
-      
-      if (docsearch[i]:get_attribute("property"):gsub("og:","") == "image") then
-        result_content = result_content .. docsearch[i]:get_attribute("content") .. "\n\n"
-      end
-      
-      if (docsearch[i]:get_attribute("property"):gsub("og:","") == "url") then
-        result_content = result_content .. docsearch[i]:get_attribute("content") .. "\n\n"
-      end
-
-      print("---")
-    end
-  end
-
-  -- test for html5 article tags
-  local docsearch = doc:search("//article//p")
-  local err, val = pcall(function() return docsearch[1]:name() end)
-  if (#docsearch ~=0) then
-    for i,t in ipairs(docsearch) do    
-      result_content = result_content .. docsearch[i]:content() .. "\n\n"
-    end
-  end
-  
-  -- test for html5 subsection
-  local docsearch = doc:search("//subsection")
-  local err, val = pcall(function() return docsearch[1]:name() end)
-  if (err == false) then
-    if (verbose) then
-       print("subsection:")
-       print(val)
-    end
-  end
-  
-  -- test for aria tags
-  -- TODO: map aria annotations to relevant roles
-  local docsearch = doc:search("//*[@aria-description]")
-  local err, val = pcall(function() return docsearch[1]:name() end)
-  if (err == false) then
-    if (verbose) then
---      print("aria-description:")
---      print(val)
-    end
-  end
-  
-  local docsearch = doc:search("//*[@aria-label]")
-  local err, val = pcall(function() return docsearch[1]:name() end)
-  if (err == false) then
-    if (verbose) then
---      print("aria-label:")
---      print(val)
-    end
-  end
-
-  return "", ""
+  return result_content, result_xml
 end
 
 function find_query(url, db)
+  local logger = Logging.new()
   handle = sqlite3.open(db)
   u = url_parser.parse(url)
   for path,query,map,layout in handle:urows("SELECT path,query,map,layout FROM uri WHERE host='"..u.host.."';") do
---    print(u.path)
+  logger:info(u.path)
     if u.path:match(path) then
---      print("PATH MATCHES")
---      print(path,query,map,layout)
+      logger:info("PATH MATCHES")
+--      logger:info(path,query,map,layout)
       return query
     end
   end
